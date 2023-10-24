7477ee2de186ce8a9ac57327704e2e9225eeffed
minhvo0o <vdminh@penphy.edu.vn>
2023-10-24 17:07:25 +0700

    fix bug

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-slate text-primary-foreground shadow-md hover:bg-primary/90',
+          'bg-primary text-primary-foreground shadow-md hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
