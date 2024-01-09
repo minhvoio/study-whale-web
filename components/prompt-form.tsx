061dc3028f6800419b13bb843fb22f677b539baf
minhvo0o <vdminh@penphy.edu.vn>
2024-01-09 09:40:52 +0700

    Update max height of prompt form

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -49,7 +49,7 @@ export function PromptForm({
       }}
       ref={formRef}
     >
-      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+      <div className="relative flex max-h-80 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
         <Textarea
           ref={inputRef}
           tabIndex={0}
